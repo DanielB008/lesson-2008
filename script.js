f45a2c0e99b8765ef52e8f4bbf7d44c03c317a65
Eldiyar <bakirovdaniel5@gmail.com>
2025-02-12 16:12:33 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert("Hello World!");
+
+var name = "Daniel"
+var age = 16
+
+console.log(name,age)
+
+var name = prompt("Enter your name")
+var age = prompt("Enter your age")
+var pasword = prompt("Enter your password")
+
+alert(name,age,pasword)
+
+
+
